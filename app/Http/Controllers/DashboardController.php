c63d26124e3e23eddccff8978db79f1ad4974093
handikatriarlan <handikaarlan@gmail.com>
2025-01-07 01:23:11 +0700

    feat(dashboard): add more data counts to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Alumni;
 use App\Models\Peserta;
+use Illuminate\Http\Request;
+use App\Models\ProdukInovasi;
+use App\Models\TenagaPendidik;
+use App\Models\JumlahMahasiswa;
+use App\Models\PublikasiPenelitian;
+use App\Http\Controllers\Controller;
+use App\Models\PengabdianMasyarakat;
+use App\Models\JumlahMahasiswaBerprestasi;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $data = [
-            "count_pesereta" => Peserta::count(),
+            "count_peserta" => Peserta::count(),
+            "count_admin" => User::where('role', 'super_admin')->count(),
+            "count_jumlah_mahasiswa" => JumlahMahasiswa::count(),
+            "count_jumlah_mahasiswa_berprestasi" => JumlahMahasiswaBerprestasi::count(),
+            "count_tenaga_pendidik" => TenagaPendidik::count(),
+            "count_publikasi_penelitian" => PublikasiPenelitian::count(),
+            "count_produk_inovasi" => ProdukInovasi::count(),
+            "count_pengabdian_masyarakat" => PengabdianMasyarakat::count(),
+            "count_alumni" => Alumni::count(),
             "count_registered" => Peserta::where('status_daftar', 1)->count(),
         ];
-    
+
         return view('index', $data);
     }
 }
